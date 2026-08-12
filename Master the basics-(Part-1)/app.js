134cc676c9b32060c698992b236ee959d8448068
Nitin Purohit <nitinpurohit412@gmail.com>
2026-08-12 16:18:07 +0530

    First lecture on DSA

---
+++ Master the basics-(Part-1)/app.js
@@ -0,0 +1,44 @@
+//? Basics
+
+//# Problems - 
+
+//? Q-1
+// let i = 10;
+// i = i++ + ++i
+// console.log(i)
+
+
+//? Q-2
+// let a = 11 , b = 22
+// let c = a +b + a++ + b++ + ++a + ++b
+// console.log("a =" + a)
+// console.log("b =" + b)
+// console.log("c =" + c)
+
+//? Q-3 
+// let a = true //![In JS true is takes as "1"] so answer = 2
+// a++
+// console.log(a);
+
+//? Q-4  Calculate area and perimeter of reactangle
+// let a = 4
+// let b =5
+
+// console.log("Area of rectangle = " + (a*b))
+// console.log("Perimeter of rectangle = " + (2*(a+b)))
+
+//? Q-5 Generate OTP
+// console.log(Math.trunc(Math.random()*9000)+1000)
+
+//? Q-6 Area of triangle by heron's formula
+// let a = 5
+// let b =4
+// let c= 3
+
+// let s = (a+b+c)/2
+
+// console.log(Math.sqrt(s* (s-a) * (s-b) * (s-c) ));
+
+//? Q-7 Circumference of circle 
+// let r = 14
+// console.log((2*Math.PI*r).toFixed(2));
