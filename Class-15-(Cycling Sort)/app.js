b8c2d07dc92ef77255209434db6ac6702c713ebd
Nitin Purohit <nitinpurohit412@gmail.com>
2026-09-11 13:28:00 +0530

    Cycling sort

---
+++ Class-15-(Cycling Sort)/app.js
@@ -0,0 +1,15 @@
+//*  Cycling Sort -
+
+// let arr = [1,6,5,9,8,3,4,7,2]
+// let i =0 
+// while(i<arr.length){
+//     let correctIdx = arr[i] - 1
+//     if(arr[i] != arr[correctIdx]){
+//         let temp = arr[i]
+//         arr[i] = arr[correctIdx]
+//         arr[correctIdx] = temp
+//     }
+//     else i++
+// }
+
+// console.log(arr)
